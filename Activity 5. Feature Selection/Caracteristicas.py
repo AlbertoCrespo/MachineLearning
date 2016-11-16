5ed8b455d18e0e260d63c847ac01176e6a0f8ca8
alberto <Alberto.Crespo4@alu.uclm.es>
2016-11-16 13:03:47 +0100

    16/11/2016 13:05

--- Activity 5. Feature Selection/Caracteristicas.py
+++ Activity 5. Feature Selection/Caracteristicas.py
@@ -13,6 +13,7 @@ def iniciar(lista):
 
 lista_de_cluster = []
 Numero_accidentes_por_cluster = []
+
 Numero_de_accidentes_por_Salida = []
 Numero_de_accidentes_por_Salida = iniciar(Numero_de_accidentes_por_Salida)
 Numero_de_accidentes_por_Alcance = []
@@ -24,10 +25,20 @@ Numero_de_accidentes_por_Tijera = iniciar(Numero_de_accidentes_por_Tijera)
 Numero_de_accidentes_por_Atropello= []
 Numero_de_accidentes_por_Atropello = iniciar(Numero_de_accidentes_por_Atropello)
 
+Numero_de_accidentes_por_Nivel_Amarillo = []
+Numero_de_accidentes_por_Nivel_Amarillo = iniciar(Numero_de_accidentes_por_Nivel_Amarillo)
+Numero_de_accidentes_por_Nivel_Blanco = []
+Numero_de_accidentes_por_Nivel_Blanco = iniciar(Numero_de_accidentes_por_Nivel_Blanco)
+Numero_de_accidentes_por_Nivel_Negro = []
+Numero_de_accidentes_por_Nivel_Negro = iniciar(Numero_de_accidentes_por_Nivel_Negro)
+Numero_de_accidentes_por_Nivel_Rojo = []
+Numero_de_accidentes_por_Nivel_Rojo = iniciar(Numero_de_accidentes_por_Nivel_Rojo)
+
+
 
 
 archivo=open ("Carectiristicas.csv","a")
-archivo.write("Cluster,Numero de Accidentes por Cluster,Numero de accidentes por salida, numero de accidentes por Alcance, Numero de accidentes por Vuelco, Numero de accidentes por tijera, numero de accidentes por atropello")
+archivo.write("Cluster,Numero de Accidentes por Cluster,Numero de accidentes por salida, numero de accidentes por Alcance, Numero de accidentes por Vuelco, Numero de accidentes por tijera, numero de accidentes por atropello, Numero de accidentes por nivel Amarillo, Numero de Accidentes por Nivel Blanco, Numero de accidentes por nivel Negro, Numero de accidentes por nivel Rojo")
 archivo.write("\n")
 
 
@@ -72,6 +83,26 @@ cursor.execute("SELECT cluster,count(causa) FROM ACCIDENTES where causa='Atropel
 for i in cursor:
 	Numero_de_accidentes_por_Atropello[int(i[0]+1)] = str(i[1])
 
+#Distitntos tipos de acccidentes por Nivel Amarillo
+cursor.execute("SELECT cluster,count(nivel) FROM ACCIDENTES where nivel='Amarillo' GROUP BY cluster")
+for i in cursor:
+	Numero_de_accidentes_por_Nivel_Amarillo[int(i[0]+1)] = str(i[1])
+
+#Distitntos tipos de acccidentes por Nivel Blanco
+cursor.execute("SELECT cluster,count(nivel) FROM ACCIDENTES where nivel='Blanco' GROUP BY cluster")
+for i in cursor:
+	Numero_de_accidentes_por_Nivel_Blanco[int(i[0]+1)] = str(i[1])
+
+#Distitntos tipos de acccidentes por Nivel Negro
+cursor.execute("SELECT cluster,count(nivel) FROM ACCIDENTES where nivel='Negro' GROUP BY cluster")
+for i in cursor:
+	Numero_de_accidentes_por_Nivel_Negro[int(i[0]+1)] = str(i[1])
+
+#Distitntos tipos de acccidentes por Nivel Rojo
+cursor.execute("SELECT cluster,count(nivel) FROM ACCIDENTES where nivel='Rojo' GROUP BY cluster")
+for i in cursor:
+	Numero_de_accidentes_por_Nivel_Rojo[int(i[0]+1)] = str(i[1])
+
 
 cursor.close()
 con.close()
@@ -79,7 +110,7 @@ con.close()
 
 x = 0
 for i in lista_de_cluster:
-	archivo.write(str(i)+","+str(Numero_accidentes_por_cluster[x])+","+str(Numero_de_accidentes_por_Salida[x])+","+str(Numero_de_accidentes_por_Alcance[x])+","+str(Numero_de_accidentes_por_Vuelco[x])+","+str(Numero_de_accidentes_por_Tijera[x])+","+str(Numero_de_accidentes_por_Atropello[x]))
+	archivo.write(str(i)+","+str(Numero_accidentes_por_cluster[x])+","+str(Numero_de_accidentes_por_Salida[x])+","+str(Numero_de_accidentes_por_Alcance[x])+","+str(Numero_de_accidentes_por_Vuelco[x])+","+str(Numero_de_accidentes_por_Tijera[x])+","+str(Numero_de_accidentes_por_Atropello[x])+","+str(Numero_de_accidentes_por_Nivel_Amarillo[x])+","+str(Numero_de_accidentes_por_Nivel_Blanco[x])+","+str(Numero_de_accidentes_por_Nivel_Negro[x])+","+str(Numero_de_accidentes_por_Nivel_Rojo[x]))
 	archivo.write("\n")
 	x = x + 1
 
